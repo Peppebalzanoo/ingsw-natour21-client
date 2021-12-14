ad8554db3d8f46271dd8438585e131c6d578e6f9
Peppebalzanoo <giuseppe.balzano7@studenti.unina.it>
2021-12-14 15:55:57 +0100

    Funzionalità aggiunte: 1. Cambio/Modifica immagine profilo utente 2. Richiesta/Controllo permessi di READ/WRITE_EXTERNAL_STORAGE 3. Salvataggio/Compressione in locale immagine profilo utente

--- app/src/main/java/com/example/natour2/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/natour2/fragment/ProfileFragment.java
@@ -1,23 +1,49 @@
 package com.example.natour2.fragment;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.natour2.R;
 import com.example.natour2.adapter.ItinerarioAdapter;
 import com.example.natour2.controller.ControllerHomeAcrtivity;
 import com.example.natour2.model.Itinerario;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,6 +56,12 @@ public class ProfileFragment extends Fragment {
     private RecyclerView recyclerView;
     private ItinerarioAdapter itinerarioAdapter;
     private List<Itinerario> itinerarioList;
+
+    /* ****************************************************************************************** */
+    private CircleImageView image = null;
+    private Bitmap bitmapApp = null;
+    private byte arrayBytesOfImageProfile[] = null;
+    /* ****************************************************************************************** */
 
     private final ControllerHomeAcrtivity ctrl = new ControllerHomeAcrtivity();
 
@@ -82,14 +114,35 @@ public class ProfileFragment extends Fragment {
 
         recyclerView = view.findViewById(R.id.recycler_view2);
         recyclerView.setHasFixedSize(true);
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         linearLayoutManager.setReverseLayout(true);
         linearLayoutManager.setStackFromEnd(true);
+
         recyclerView.setLayoutManager(linearLayoutManager);
+
         itinerarioList = new ArrayList<>();
         itinerarioAdapter = new ItinerarioAdapter(getContext(), itinerarioList);
+
         recyclerView.setAdapter(itinerarioAdapter);
 
+
+        /* ****************************************************************************************/
+        image = view.findViewById(R.id.imageViewProfile);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean permission1 = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+                Boolean permission2 = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+                if(permission1 && permission2){
+                    selectImageFromGallery();
+                }else {
+                    CheckPermission(permission1, permission2);
+                }
+            }
+        });
+        /* ****************************************************************************************/
         readItinerari();
         return view;
     }
@@ -102,5 +155,109 @@ public class ProfileFragment extends Fragment {
 
         itinerarioAdapter.notifyDataSetChanged();
     }
+
+    /* ****************************************************************************************** */
+    public void CheckPermission(Boolean perm1, Boolean perm2){
+        if(perm1 == perm2){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 4);
+        }else if(perm1){
+            ActivityCompat.requestPermissions(getActivity(),new String[] { Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+        }else{
+            ActivityCompat.requestPermissions(getActivity(),new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, 3);
+        }
+    }
+
+    public void selectImageFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, RESULT_OK);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 2:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    selectImageFromGallery();
+                }else{
+                    Toast.makeText(getContext(), "Abilita il permesso 'READ_EXTERNAL_STORAGE'", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 3:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    selectImageFromGallery();
+                }else{
+                    Toast.makeText(getContext(), "Abilita il permesso 'WRITE_EXTERNAL_STORAGE'", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case 4:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    selectImageFromGallery();
+                }else{
+                    Toast.makeText(getContext(), "Abilita i permessi 'WRITE_EXTERNAL_STORAGE' e 'READ_EXTERNAL_STORAGE'", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            try {
+                Uri selectedImageUri = data.getData();
+                image.setImageURI(selectedImageUri);
+                bitmapApp = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), selectedImageUri);
+                arrayBytesOfImageProfile = convertBitmapToArrayOfByte(bitmapApp);
+                createDirectoryAndSaveFile(convertByteToBitmap(arrayBytesOfImageProfile), "NaTour21_IMG");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void createDirectoryAndSaveFile(Bitmap imageToSave, String fileName){
+        OutputStream outStream = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            try {
+                ContentResolver resolver = getContext().getContentResolver();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName + ".jpeg");
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+                contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+                Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+                outStream = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                String directoryImg = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+                File image = new File(directoryImg, fileName + ".jpeg");
+                outStream = new FileOutputStream(image);
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+            Objects.requireNonNull(outStream).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bitmap convertByteToBitmap(byte array[]){
+        Bitmap bitmap = BitmapFactory.decodeByteArray(array , 0, array.length);
+        return bitmap;
+    }
+
+    public byte[] convertBitmapToArrayOfByte(Bitmap bit){
+        ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
+        bit.compress(Bitmap.CompressFormat.JPEG, 100, byteOutStream);
+        return byteOutStream.toByteArray();
+    }
+    /* ****************************************************************************************** */
+
 
 }
