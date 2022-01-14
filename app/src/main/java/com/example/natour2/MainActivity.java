af9e47d0152d943ce2a335bd95b73325ce81a366
Peppebalzanoo <giuseppe.balzano7@studenti.unina.it>
2022-01-14 02:16:35 +0100

    Commit

--- app/src/main/java/com/example/natour2/MainActivity.java
+++ app/src/main/java/com/example/natour2/MainActivity.java
@@ -6,19 +6,6 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-/*
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.model.CreatePlatformEndpointRequest;
-import com.amazonaws.services.sns.model.CreatePlatformEndpointResult;
-import com.amazonaws.services.sns.model.PublishRequest;
-*/
-/*import com.amplifyframework.AmplifyException;
-import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
-import com.amplifyframework.core.Amplify;*/
 import com.example.natour2.controller.ControllerLoginSignin;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -64,34 +51,32 @@ public class MainActivity extends AppCompatActivity {
                         }
                         // Get new FCM registration token
                         Firebasetoken = task.getResult();
-                        Log.i("FIREBASETOKEN", "******************************************* FIREBASE_TOKEN: "+ Firebasetoken);
+                        Log.i("FIREBASETOKEN", "******************************************* FIREBASE_TOKEN: " + Firebasetoken);
                     }
                 });
 
 
 
-
-        String AWS_ACCESS_KEY = "AKIATQAAFEJQKY6S7ATV ";
-        String AWS_SECRET_KEY = "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";
-        String PLATFORM_APPLICATION_ARN = "arn:aws:sns:us-east-1:240518505056:app/GCM/NaTour21";
-
-        /*
-        AmazonSNS snsClient = AmazonSNSClient.builder().withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY))).build();
-        String arn  = null;
-
-        CreatePlatformEndpointRequest createPlatformEndpointRequest = new CreatePlatformEndpointRequest().withCustomUserData("username").withToken("tokenfirebase").withPlatformApplicationArn(PLATFORM_APPLICATION_ARN);
-        CreatePlatformEndpointResult platformEndpoint = snsClient.createPlatformEndpoint(createPlatformEndpointRequest);
-        arn = platformEndpoint.getEndpointArn();
-
-        //AmazonSNS snsClient = AmazonSNSClient.builder().withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY))).build();
-
-        try {
-            PublishRequest publishReq = new PublishRequest().withMessage("message").withTargetArn(arn);
-            snsClient.publish(publishReq);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        */
-
+        /* ************************************************************************************** */
+//        String AWS_ACCESS_KEY = "AKIATQAAFEJQKY6S7ATV ";
+//        String AWS_SECRET_KEY = "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";
+//        String PLATFORM_APPLICATION_ARN = "arn:aws:sns:us-east-1:240518505056:app/GCM/NaTour21";
+//
+//        AmazonSNS snsClient = AmazonSNSClient.builder().withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY))).build();
+//        String firebase_token_device_1 = "";
+//        String firebase_token_device_2 = "";
+//        CreatePlatformEndpointRequest createPlatformEndpointRequest = new CreatePlatformEndpointRequest().withCustomUserData("username").withToken(firebase_token_device_2).withPlatformApplicationArn(PLATFORM_APPLICATION_ARN);
+//        CreatePlatformEndpointResult platformEndpoint = snsClient.createPlatformEndpoint(createPlatformEndpointRequest);
+//        String ARNEndPoint = platformEndpoint.getEndpointArn();
+//
+//
+//        try {
+//            PublishRequest publishReq = new PublishRequest().withMessage("message").withTargetArn(ARNEndPoint);
+//            snsClient.publish(publishReq);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        /* ************************************************************************************** */
     }
+
 }
